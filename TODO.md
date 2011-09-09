c331068be51699f4b7b1c4143190519f9b86805d
John Crepezzi <john.crepezzi@gmail.com>
2011-09-09 17:00:29 -0400

    Tests for ExpirationTracker

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 SOONISH
 * strip out some query parameters in order to avoid cache misses (oauth_nonce, etc)
-* more tests, starting in ExpirationTracker
 
 DISTANCE
 * add try again in.. header
--- spec/expiration_tracker_spec.js
+++ spec/expiration_tracker_spec.js
@@ -7,50 +7,94 @@ describe('ExpirationTracker', function() {
     cache_manager.flush();
   });
 
-  it('should start tracking at 0', function() {
-    var tracker = new ExpirationTracker('hello', 0, 10);
-    runs(function() {
-      var _this = this;
-      tracker.usesInInterval(function(num, total) {
-        expect(num).toBe(0);
-        _this.completed = true;
+  describe('usesInInterval', function() {
+
+    it ('should decrement automatically over time', function() {
+      var tracker = new ExpirationTracker('hello', 1, 0.05);
+      tracker.used();
+      waits(100); // fall out of bounds
+      runs(function() {
+        var _this = this;
+        tracker.usesInInterval(function(num, total) {
+          expect(num).toBe(0);
+          _this.completed = true;
+        });
+      });
+      waitsFor(function() {
+        return this.completed;
       });
     });
-    waitsFor(function() {
-      return this.completed;
-    });
+
   });
 
-  it('be able to track a use', function() {
-    var tracker = new ExpirationTracker('hello', 10, 10);
-    tracker.used();
-    runs(function() {
-      var _this = this;
-      tracker.usesInInterval(function(num, total) {
-        expect(num).toBe(1);
-        expect(total).toBe(10);
-        _this.completed = true;
+  describe('clear', function() {
+    
+    it ('should be able to clear for a tracking key', function() {
+      var tracker = new ExpirationTracker('hello', 10, 10);
+      tracker.used();
+      tracker.clear();
+      runs(function() {
+        var _this = this;
+        tracker.usesInInterval(function(num, total) {
+          expect(num).toBe(0);
+          _this.completed = true;
+        });
+      });
+      waitsFor(function() {
+        return this.completed;
       });
     });
-    waitsFor(function() {
-      return this.completed;
-    });
+
   });
 
-  it('should continue to track over the line', function() {
-    var tracker = new ExpirationTracker('hello', 0, 1);
-    tracker.used();
-    runs(function() {
-      var _this = this;
-      tracker.usesInInterval(function(num, total) {
-        expect(num).toBe(1);
-        expect(total).toBe(0);
-        _this.completed = true;
+  describe('used', function() {
+
+    it('should start tracking at 0', function() {
+      var tracker = new ExpirationTracker('hello', 0, 10);
+      runs(function() {
+        var _this = this;
+        tracker.usesInInterval(function(num, total) {
+          expect(num).toBe(0);
+          _this.completed = true;
+        });
+      });
+      waitsFor(function() {
+        return this.completed;
       });
     });
-    waitsFor(function() {
-      return this.completed;
+
+    it('be able to track a use', function() {
+      var tracker = new ExpirationTracker('hello', 10, 10);
+      tracker.used();
+      runs(function() {
+        var _this = this;
+        tracker.usesInInterval(function(num, total) {
+          expect(num).toBe(1);
+          expect(total).toBe(10);
+          _this.completed = true;
+        });
+      });
+      waitsFor(function() {
+        return this.completed;
+      });
     });
+
+    it('should continue to track over the line', function() {
+      var tracker = new ExpirationTracker('hello', 0, 1);
+      tracker.used();
+      runs(function() {
+        var _this = this;
+        tracker.usesInInterval(function(num, total) {
+          expect(num).toBe(1);
+          expect(total).toBe(0);
+          _this.completed = true;
+        });
+      });
+      waitsFor(function() {
+        return this.completed;
+      });
+    });
+
   });
 
   //////////////////
