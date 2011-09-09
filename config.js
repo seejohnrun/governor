521095830eaf75d15d3317df8e561cdebecd22a1
John Crepezzi <john.crepezzi@gmail.com>
2011-09-09 09:26:27 -0400

    Start putting config together

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+
+  // The general application host and port
+  host: '127.0.0.1',
+  port: 8980,
+
+  // How we should log
+  logger: {
+    type: 'Console',
+    options: {
+      colorize: true,
+      level: 'verbose'
+    }
+  }
+
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var winston = require('winston');
+var fs = require('fs');
 var ProxyServer = require('./lib/proxy_server');
+var cache_manager = require('./lib/cache_manager');
+var config = require('./config');
 
-// Initialize winston
+// Initialize winston, default stdout
 winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, { colorize: true, level: 'verbose' });
+if (config.logger) {
+  winston.add(winston.transports[config.logger.type], config.logger.options);
+} else {
+  winston.add(winston.transports.Console, { colorize: true, level: 'verbose' });
+}
+
+// Set up the cache client
+// cache_manager.config(config.cache_manager);
 
 // And create the server
-var server = new ProxyServer({ });
+var server = new ProxyServer({
+  host: config.host,
+  port: config.port
+});
 server.listenForever();
